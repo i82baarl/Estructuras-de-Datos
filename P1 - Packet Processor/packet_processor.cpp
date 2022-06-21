0704344e31d9dbb9a0681136b26e24dcb6064b8a
LuisBallesteros <55761141+i82baarl@users.noreply.github.com>
2022-06-21 19:19:01 +0200

    Add files via upload
    Lists, Stacks, Queues

---
+++ P1 - Packet Processor/packet_processor.cpp
@@ -0,0 +1,84 @@
+/**
+ * CopyRight F. J. Madrid-Cuevas <fjmadrid@uco.es>
+ *
+ * Sólo se permite el uso de este código en la docencia de las asignaturas sobre
+ * Estructuras de Datos de la Universidad de Córdoba.
+ *
+ * Está prohibido su uso para cualquier otro objetivo.
+ */
+
+#include "packet_processor.hpp"
+#include "queue_imp.hpp"
+
+PacketProcessor::PacketProcessor(size_t size):
+    _max_size(size),
+    _finish_time()
+{size_ = size;}
+
+Response PacketProcessor::process(const Packet &packet)
+{
+    Response ret (true, 0); //Default is to drop the packet.
+    //TODO
+    //First: deque all processed packets at packet time arrival.
+    //
+    //Second: Is there any place for this packet in the queue?
+    //This packet will be processed after the last paquete will do,
+    //and the its finish time will be 'packets's process_time' units after.
+    //Remenber that the queue holds the finish processing time for the packets.
+
+    int time = 0;
+
+    if(!planificador_.is_empty()){
+
+        if(packet.arrival_time >= planificador_.front()){
+
+            planificador_.deque();
+        }
+    }
+
+    if(planificador_.size() >= size_){
+
+        return Response(true, 0);
+    }
+
+    if(planificador_.is_empty()){
+
+        time = packet.arrival_time;
+    }
+
+    else{
+
+        time = planificador_.back();
+    }
+
+    planificador_.enque(time + packet.process_time);
+
+    return Response(false, time);
+
+    //
+    return ret;
+}
+
+
+/** @brief process the packets and generate a response for each of them.*/
+std::vector <Response>
+process_packets(const std::vector <Packet> &packets,
+                PacketProcessor& p)
+{
+    std::vector <Response> responses;
+    for (size_t i = 0; i < packets.size(); ++i)
+    {
+        responses.push_back(p.process(packets[i]));
+    }
+    return responses;
+}
+
+/** @brief print the processing start times for the packets.*/
+std::ostream&
+write_responses(std::ostream &out, const std::vector <Response> &responses)
+{
+    for (size_t i = 0; i < responses.size(); ++i)
+        out << (responses[i].dropped ? -1 : responses[i].start_time)
+            << std::endl;
+    return out;
+}
---
+++ P1 - Packet Processor/queue_imp.hpp
@@ -0,0 +1,147 @@
+/**
+ * CopyRight F. J. Madrid-Cuevas <fjmadrid@uco.es>
+ *
+ * Sólo se permite el uso de este código en la docencia de las asignaturas sobre
+ * Estructuras de Datos de la Universidad de Córdoba.
+ *
+ * Está prohibido su uso para cualquier otro objetivo.
+ */
+#pragma once
+
+#include <cassert>
+#include "queue.hpp"
+
+template<class T>
+Queue<T>::Queue ()
+{
+    //TODO    
+
+    input_ = Stack<T>::create();
+    output_ = Stack<T>::create();
+
+    //
+    assert(is_empty());
+}
+
+template<class T>
+bool
+Queue<T>::is_empty () const
+{
+    bool ret_val = true;
+    //TODO
+    if( !(input_->is_empty()) || !(output_->is_empty()) ){
+        ret_val = false;
+    }
+    //
+    return ret_val;
+}
+
+template<class T>
+size_t
+Queue<T>::size () const
+{
+    size_t ret_val = 0;
+    //TODO
+
+    ret_val = (size_t)input_->size() + (size_t)output_->size();
+
+    //
+    return ret_val;
+}
+
+template<class T>
+T
+Queue<T>::front() const
+{
+    assert(! is_empty());
+    T ret_val;
+    //TODO
+    if( output_->is_empty() ){
+        const_cast<Queue<T>*>(this)->flush_input_to_output();
+    }
+    ret_val = output_->top();
+
+    //
+    return ret_val;
+}
+
+template<class T>
+T Queue<T>::back() const
+{
+    assert(! is_empty());
+    T ret_val;
+    //TODO
+    if( input_->is_empty() ){
+        ret_val = back_;
+    }
+    else{
+        ret_val = input_->top();
+    }
+
+
+    //
+    return ret_val;
+}
+
+template<class T>
+void
+Queue<T>::enque(const T& new_it)
+{
+#ifndef NDEBUG
+    size_t old_size = size();
+#endif
+    //TODO
+    //Remenber we enque into the input stack.
+    //Hint: maybe you need to update the back item.
+
+    input_->push(new_it);
+    back_ = input_->top();
+
+
+    //
+    assert(back()==new_it);
+    assert(size()==(old_size+1));
+}
+
+template<class T>
+void
+Queue<T>::deque()
+{
+    assert(! is_empty());
+#ifndef NDEBUG
+    size_t old_size = size();
+#endif
+    //TODO
+    //Remenber we deque from the output stack and if the output stack is empty
+    //we need flush the input stack into the output stack first.
+    if( output_->is_empty() ){
+        flush_input_to_output();
+    }
+    output_->pop();
+
+    //
+    assert(size()==(old_size-1));
+}
+
+
+template<class T>
+void
+Queue<T>::flush_input_to_output()
+{
+    assert(!input_->is_empty());
+#ifndef NDEBUG
+    T old_back = back();
+#endif
+    //TODO
+    //Remenber: the first item pushed into output is
+    // the new back() of the queue.
+
+    while( !(input_->is_empty()) ){
+
+        output_->push( input_->top() );
+        input_->pop();
+    }
+
+    //
+    assert(old_back == back());
+}
